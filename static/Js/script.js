def1ca9178381986163c8ff2d134f401448d1a63
Ignacio-Alfredo <garridoignacio022@gmail.com>
2025-07-30 12:36:08 -0400

    aaaa

--- static/Js/script.js
+++ static/Js/script.js
@@ -1,22 +1,21 @@
 function contarPares(numero) {
     let cantidadPares = 0;
-    for (let i = 1; i <= numero; i++) {
-        if (i % 2 === 0) {
+    for (let i = 1; i <= numero; i ++){
+        if (i % 2 === 0){
             cantidadPares++;
         }
     }
     return cantidadPares;
 }
 
-function ejContarPares() {
-    const numeroIngresado = parseInt(document.getElementById('input1').value);
-    
+function ejContarPares(){
+    let numeroIngresado = parseInt(document.getElementById('input1').value);
+
     if (isNaN(numeroIngresado) || numeroIngresado < 1) {
-        alert("Por favor, ingresa un número válido mayor o igual a 1.");
-        console.log("Error: Número inválido.");
-        return;
+        console.log("Error: Número no valido.");
+        return; 
     }
 
-    const resultado = contarPares(numeroIngresado);
-    alert("La cantidad de números pares es: " + resultado);
+    let resultado = contarPares(numeroIngresado);
+    alert("La cantidad de números pares es: " + resultado)
 }
